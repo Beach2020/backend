5ec7ec14d9cc1337fe9d81d915be30e10b038912
socialexile <socialexile2019@gmail.com>
2019-03-08 08:56:52 -0700

    backend updates

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^downvote/$', views.downvote, name='downvote'),
+    url(r'^downvote-ext/$', views.downvote_ext, name='downvote_ext'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,25 +1,62 @@
 from django.shortcuts import render
 from .forms import DownVoteForm
 from .models import DownVote
-
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 
 def downvote(request):
     form = DownVoteForm(request.POST or None)
     if form.is_valid():
-        # form.save()
-        # form = DownVoteForm()
         url = request.POST.get('url')
         userId = url.split('/')[-1]
         postId = url.split('/')[-2]
-        data = DownVote(post_id=postId, user_profile=userId)
-        data.save()
+        all_data = DownVote.objects.all()
+        flag = 0
+        for data in all_data:
+            if data.user_profile == userId:
+                if data.post_id == postId:
+                    flag = 0
+                    break
+                else:
+                    flag = 1
+            else:
+                flag = 1
+        if flag == 1:
+            new_data = DownVote(post_id=postId, user_profile=userId)
+            new_data.save()
         form = DownVoteForm()
-  
-        
-    
     context = {
         'form': form
     }
     return render(request, "myapp/form_input.html", context)
+
+
+def downvote_ext(request):
+    url = request.POST.get('url')
+    userId = url.split('/')[-1]
+    postId = url.split('/')[-2]
+    all_data = DownVote.objects.all()
+    flag = 0
+    for data in all_data:
+        if data.user_profile == userId:
+            if data.post_id == postId:
+                flag = 0
+                break
+            else:
+                flag = 1
+        else:
+            flag = 1
+    if flag == 1:
+        new_data = DownVote(post_id=postId, user_profile=userId)
+        new_data.save()
+    vote_list = []
+    all_data = DownVote.objects.all()
+    for data in all_data:
+        if data.post_id == postId:
+            vote_list.append(data)
+    return HttpResponse(
+        json.dumps({'total_downvote': len(vote_list), 'msg': 'Update Success.'}),
+        content_type="application/json"
+    )
