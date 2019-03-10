15bbde82c8a8018457af27b450a1346afabac792
socialexile <socialexile2019@gmail.com>
2019-03-09 17:26:03 -0700

    professor changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url,path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^downvote/$', views.downvote, name='downvote'),
     url(r'^downvote-ext/$', views.downvote_ext, name='downvote_ext'),
-    path('d/<int:pid>/<str:uid>/', views.d, name='d'),
+    url(r'^d/?<pid>[0-9]+/?<uid>[A-Za-z0-9_]+/', views.d, name='d'),
 ]
