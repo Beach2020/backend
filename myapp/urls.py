c0f921e835752c10e68a621935c849e342894ed8
socialexile <socialexile2019@gmail.com>
2019-03-11 19:48:39 -0700

    urls update

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^downvote/$', views.downvote, name='downvote'),
     url(r'^downvote-ext/$', views.downvote_ext, name='downvote_ext'),
     url(r'^d/(?P<pid>\d+)/(?P<uid>[\w.+-]+)/$', views.d, name='d'),
-    url(r'^s/(?P<pid>\d+)/(?P<uid>[\w.+-]+)/$', views.s, name='s'),
+    url(r'^s/(?P<pid>\d+)/$', views.s, name='s'),
 ]
